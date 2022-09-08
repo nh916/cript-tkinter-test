fec7c2fe85957169f517eed510000d4514676949
Navid Hariri <navidhariri25@gmail.com>
2022-09-07 20:54:14 -0700

    not sure if inheritance is needed yet or not

---
+++ screens/Base.py
@@ -0,0 +1,14 @@
+# python package
+import tkinter
+
+# my modules
+
+
+class BaseScreen:
+    def __init__(self, root, frame):
+        # main window (root) and loading frame
+        self.root = root
+        self.frame = frame
+
+    def get_screen(self):
+        return self.frame
